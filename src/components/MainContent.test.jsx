862751a6daf30c4a7f5a28478603edc22c622bca
yasinfahmy <95018429+yasinfahmy@users.noreply.github.com>
2026-09-04 18:54:45 +0200

    added comments

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,6 +4,8 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 
+//This is a comment
+
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
