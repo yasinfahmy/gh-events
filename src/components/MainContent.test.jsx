53000bac20d3cb70ecc55b4c0ab8b48133e2071b
yasinfahmy <95018429+yasinfahmy@users.noreply.github.com>
2026-09-04 18:59:04 +0200

    added comments [skip actions]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -5,6 +5,7 @@ import userEvent from '@testing-library/user-event';
 import MainContent from './MainContent';
 
 //This is a comment
+//This is another comment
 
 describe('MainContent', () => {
   it('should render a button', () => {
